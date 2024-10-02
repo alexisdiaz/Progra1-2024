acff7cf79d55f90892ca9a45f46c1bd4646282e8
alexiszeldiaz <alexiszeldiaz@gmail.com>
2024-10-01 20:57:24 -0600

    base de datos incluida

--- academica/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[materias]
-(
-	[IdMaterias] INT IDENTITY (1, 1) NOT NULL,
-    [codigo] CHAR(10) NOT NULL, 
-    [nombre] CHAR(75) NOT NULL, 
-    [uv] CHAR(10) NOT NULL
-	PRIMARY KEY CLUSTERED ([IdMaterias] ASC)
-)
